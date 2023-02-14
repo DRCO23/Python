d0d10f36d1c565eeffee22532cda1bd5c7bde0e4
Murilo <125324603+DRCO23@users.noreply.github.com>
2023-02-13 22:44:16 -0300

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,13 @@
+print ("Olá, esté é um programa de calculadora, informe os valores abaixo!")
+numero1 =  float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+soma= numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+
+print("A soma desses números é:" +str(soma))
+print ("A subtração desses números é:" +str(subtracao))
+print ("A multiplicação desses números é:" +str(multiplicacao))
+print ("A divisão desses números é:" +str (divisao))
